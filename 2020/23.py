ce02c0ec2940a08d69fa867be2f2866d0d217308
Ivan Zlatev <ivanzlatev90@gmail.com>
2020-12-23 21:13:03 +0200

    2020/23/part2

--- 2020/23.py
+++ 2020/23.py
@@ -7,39 +7,77 @@ import sys
 
 sys.setrecursionlimit(9999)
 
+cups = dict()
+
 def main(data):
-   cups = [int(x) for x in data]
+   global cups
+   for i in range(len(data)):
+      x = int(data[i])
+      if i < len(data)-1:
+         cups[x] = int(data[i+1])
+      else:
+         cups[x] = int(data[0])
+   index = int(data[0])
    for i in range(100):
-      cups = playRound(cups)
-   oneIndex = cups.index(1)
-   result = cups[oneIndex+1:] + cups[:oneIndex]
+      index = playRound(index)
+   plusOne = cups[index]
+   plusTwo = cups[plusOne]
+   result = []
+   index = 1
+   for i in range(1, len(cups)):
+      result.append(str(cups[index]))
+      index = cups[index]
    print "Q1: {}".format(''.join([str(x) for x in result]))
-   cups = [int(x) for x in data]
-   for i in range(max(cups), 1000000):
-      cups.append(i+1)
+   #return 0
+   cups = dict()
+   for i in range(len(data)):
+      x = int(data[i])
+      if i < len(data)-1:
+         cups[x] = int(data[i+1])
+      else:
+         cups[x] = len(data)+1
+   for i in range(len(data)+1, 1000001):
+      if i < 1000000:
+         cups[i] = i+1
+      else:
+         cups[i] = int(data[0])
+   index = int(data[0])
    for i in range(10000000):
-      if i > 0 and i%100 == 0:
-         print i
-      cups = playRound(cups)
-   oneIndex = cups.index(1)
-   result = cups[oneIndex+1] * cups[oneIndex+2]
+      index = playRound(index)
+   plusOne = cups[1]
+   plusTwo = cups[plusOne]
+   result = plusOne * plusTwo
    print "Q2: {}".format(result)
 
-def playRound(cups):
-   currentCup = cups[0]
-   removedCups = cups[1:4]
-   cups = cups[4:]
-   nextCup = currentCup-1
+def playRound(index):
+   global cups
+   #print index, cups
+   plusOne = cups[index]
+   plusTwo = cups[plusOne]
+   plusThree = cups[plusTwo]
    try:
-      nextCup = max([x for x in cups if x <= nextCup])
+      if index-1 > 0:
+         if index-1 not in [index, plusOne, plusTwo, plusThree]:
+            destination = index-1
+         elif index-2 > 0 and index-2 not in [index, plusOne, plusTwo, plusThree]:
+            destination = index-2
+         elif index-3 > 0 and index-3 not in [index, plusOne, plusTwo, plusThree]:
+            destination = index-3
+         else:
+            destination = max([x for x in cups.keys() if x not in [index, plusOne, plusTwo, plusThree] and x < index])
+      else:
+         #print "I'm in try else, index is {}, plusOne {}, plusTwo {}, plusThree {}".format(index, plusOne, plusThree, plusThree)
+         destination = max([x for x in cups.keys() if x not in [index, plusOne, plusTwo, plusThree] and x < index])
    except:
-      nextCup = max(cups)
-   nextCupIndex = cups.index(nextCup)+1
-   cups = cups[:nextCupIndex] + removedCups + cups[nextCupIndex:] + [currentCup]
-   return cups
+      #print "I'm in except"
+      destination = max([x for x in cups.keys() if x not in [index, plusOne, plusTwo, plusThree]])
+   cups[index] = cups[plusThree]
+   cups[plusThree] = cups[destination]
+   cups[destination] = plusOne
+   return cups[index]
 
 inputData = '389125467'
-main(inputData)
+#main(inputData)
 
 inputData = '326519478'
 main(inputData)
