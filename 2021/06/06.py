a498a90e5db88687b1f2a9cae758f05b0b4170ff
Ivan Zlatev <ivanzlatev90@gmail.com>
2021-12-06 16:37:46 +0200

    optimized

--- 2021/06/06.py
+++ 2021/06/06.py
@@ -1,25 +1,54 @@
 #!/usr/bin/python3
 
 def puzzle1(data):
-    for i in range(80):
-        data = simulateDay(data)
-    return len(data)
+    fishes = {
+        0:0,
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0,
+        7:0,
+        8:0
+    }
+    for i in data:
+        fishes[i] += 1
+    for day in range(80):
+        fishes = simulateDay(fishes)
+    return sum(fishes.values())
 
 def puzzle2(data):
-    for i in range(256):
-        if i > 100:
-            print(i)
-        data = simulateDay(data)
-    return len(data)
+    fishes = {
+        0:0,
+        1:0,
+        2:0,
+        3:0,
+        4:0,
+        5:0,
+        6:0,
+        7:0,
+        8:0
+    }
+    for i in data:
+        fishes[i] += 1
+    for day in range(256):
+        fishes = simulateDay(fishes)
+    return sum(fishes.values())
 
-def simulateDay(data):
-    for i in range(len(data)):
-        if data[i] == 0:
-            data[i] = 6
-            data.append(8)
-        else:
-            data[i] -= 1
-    return data
+def simulateDay(fishes):
+    fishesUpdated = {
+        0:fishes[1],
+        1:fishes[2],
+        2:fishes[3],
+        3:fishes[4],
+        4:fishes[5],
+        5:fishes[6],
+        6:fishes[0]+fishes[7],
+        7:fishes[8],
+        8:fishes[0]
+    }
+    return fishesUpdated
 
 def readData(inputFile):
     file1 = open(inputFile, 'r')
