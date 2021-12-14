234de3433ca5a07aabe3e37a105ea2539c3e7570
Ivan Zlatev <ivanzlatev90@gmail.com>
2021-12-14 17:13:53 +0200

    2021 day 14 part 2

--- 2021/14/14.py
+++ 2021/14/14.py
@@ -3,56 +3,50 @@
 from time import perf_counter_ns
 import copy
 
-def puzzle1(data, template):
-    counts = {}
+def puzzle1(rules, template):
+    pairs = { pair: 0 for pair in rules }
+    chars = { char: template.count(char) for char in rules.values() }
+    for i in range(len(template)-1):
+        pairs[template[i:i+2]] += 1
     for i in range(10):
-        template = insertPolyPairs(data, template)
-    for i in template:
-        if i in counts.keys():
-            counts[i] += 1
-        else:
-            counts[i] = 1
-    return max(counts.values())-min(counts.values())
+        pairs, chars = step(rules, pairs.copy(), chars.copy())
+    return max(chars.values())-min(chars.values())
 
-def puzzle2(data, template):
-    counts = {}
+def puzzle2(rules, template):
+    pairs = { pair: 0 for pair in rules } # checking 2 rows down instead of pair: template.count(pair), because of FFF edge case -> count does not count overlapping substrings
+    chars = { char: template.count(char) for char in rules.values() }
+    for i in range(len(template)-1):
+        pairs[template[i:i+2]] += 1
     for i in range(40):
-        #print(template)
-        template = insertPolyPairs(data, template)
-    for i in template:
-        if i in counts.keys():
-            counts[i] += 1
-        else:
-            counts[i] = 1
-    return max(counts.values())-min(counts.values())
+        pairs, chars = step(rules, pairs, chars)
+    return max(chars.values())-min(chars.values())
 
-def insertPolyPairs(data, template):
-    result = template[0]
-    for i in range(0, len(template)-1):
-        key = template[i:i+2]
-        if key in data.keys():
-            result += data[key]
-        result += template[i+1]
-    return result
+def step(rules, pairs, chars):
+    for pair, value in pairs.copy().items():
+        pairs[pair] -= value # break existing pairs
+        pairs[pair[0] + rules[pair]] += value # create new pair for every [N-1, N] pair, where N is the new char
+        pairs[rules[pair] + pair[1]] += value # create new pair for every [N, N+1] pair, where N is the new char
+        chars[rules[pair]] += value # track and increment the number of characters
+    return pairs, chars
 
 def readData(inputFile):
     file1 = open(inputFile, 'r')
     inputData = file1.readlines()
     file1.close()
-    data = {}
+    rules = {}
     template = inputData[0].strip()
     for i in inputData:
         if '->' in i:
             pair, element = [x for x in i.strip().split(' -> ')]
-            data[pair] = element
+            rules[pair] = element
     START = perf_counter_ns()
-    result = puzzle1(copy.deepcopy(data), template)
+    result = puzzle1(copy.deepcopy(rules), template)
     time = (perf_counter_ns()-START)/1000000.0
     print("Puzzle 1 result is {} and it took {:.3f} ms".format(result, time))
     START = perf_counter_ns()
-    result = puzzle2(copy.deepcopy(data), template)
+    result = puzzle2(copy.deepcopy(rules), template)
     time = (perf_counter_ns()-START)/1000000.0
     print("Puzzle 2 result is {} and it took {:.3f} ms\n".format(result, time))
 
-#readData("test.txt")
+readData("test.txt")
 readData("input.txt")
