d07aa6f869dc9c3434e0d46af421d1355a51935d
Ivan Zlatev <ivanzlatev90@gmail.com>
2021-12-17 13:28:29 +0200

    2021 day 17 -> could use improvements, no recursion required, can be changed with while loop and the targets can be increased for versatility

---
+++ 2021/17/17.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+from time import perf_counter_ns
+import copy
+
+def puzzle1(data):
+    print(data)
+    highest = []
+    for x in range(100):
+        for y in range(-100, 100):
+            result, maxY = simulateLaunch(data, x, y)
+            if result:
+                highest.append(maxY)
+                #print('xVel =', x, 'yVel =', y, 'HIGHEST y =', maxY)
+    return max(highest)
+
+def puzzle2(data):
+    print(data)
+    valid = 0
+    for x in range(1000):
+        for y in range(-100, 100):
+            if simulateLaunch(data, x, y)[0]:
+                valid += 1
+                #print('xVel =', x, 'yVel =', y)
+    return valid
+
+def simulateLaunch(data, xVel, yVel, x=0, y=0, maxY=0):
+    #print(xVel, yVel, x, y)
+    maxY = max(maxY, y) # save current step to the list for logging
+    x += xVel # increment the x by one velocity
+    y += yVel # increment the x by one velocity
+    if xVel > 0: # due to drag, xVel -> 0
+        xVel -= 1
+    elif xVel < 0:
+        xVel += 1
+    yVel -= 1 # due to gravitiy yVel -= 1
+    if x >= min(data['x']) and x <= max(data['x']) and y >= min(data['y']) and y <= max(data['y']): # if current step is in the area -> return True and the steps log
+        return True, maxY
+    else:
+        if xVel == 0 and (x < min(data['x']) or x > max(data['x'])): # x out of bounds with xVel == 0
+            #print('x out of bounds')
+            return False, maxY
+        elif x > max(data['x']): # overshot x
+            #print('overshot x')
+            return False, maxY
+        elif y < min(data['y']): # y out of bounds, below the target area
+            #print('y below target area')
+            return False, maxY
+        else:
+            return simulateLaunch(data, xVel, yVel, x, y, maxY)
+
+def checkIfProbeInArea(x, y, xRange, yRange):
+    xInRange = x >= min(xRange) and x <= max(xRange)
+    yInRange = y >= min(yRange) and y <= max(yRange)
+    return xInRange and yInRange
+
+def readData(inputFile):
+    file1 = open(inputFile, 'r')
+    inputData = file1.readlines()
+    file1.close()
+    data = {}
+    inputData = inputData[0].strip().split(': ')[1]
+    for i in inputData.split(', '):
+        tmp = i.split('=')
+        data[tmp[0]] = [int(x) for x in tmp[1].split('..')]
+    START = perf_counter_ns()
+    result = puzzle1(copy.deepcopy(data))
+    time = (perf_counter_ns()-START)/1000000.0
+    print("Puzzle 1 result is {} and it took {:.3f} ms".format(result, time))
+    START = perf_counter_ns()
+    result = puzzle2(copy.deepcopy(data))
+    time = (perf_counter_ns()-START)/1000000.0
+    print("Puzzle 2 result is {} and it took {:.3f} ms\n".format(result, time))
+
+readData("test.txt")
+readData("input.txt")
